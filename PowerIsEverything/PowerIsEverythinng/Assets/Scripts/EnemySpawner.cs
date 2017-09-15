a639dfde47cd97b30c37fdd25e83f8758009170d
stan lewry <stan lewry>
2017-09-15 14:48:46 +0100

    EnemySpawner now uses Dela-Time. Spawn times can be altered by changing spawnTimer value - currently set to 4 seconds

--- PowerIsEverything/PowerIsEverythinng/Assets/Scripts/EnemySpawner.cs
+++ PowerIsEverything/PowerIsEverythinng/Assets/Scripts/EnemySpawner.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour {
 
+	//how many seconds between enemy spawns
+	public float spawnTimer;
+	private float spawnDeltaTime;
+
     public Rigidbody enemy;
     int count;
     Vector3 edgeVector;
@@ -16,16 +20,20 @@ public class EnemySpawner : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (count > 100) {
-            Debug.Log(edgeVector);
+
+		spawnDeltaTime += Time.deltaTime;
+
+		if (spawnDeltaTime >= spawnTimer) {
+            
+			spawnDeltaTime = 0;
+
+			Debug.Log(edgeVector);
 
             Rigidbody enemyclone = Instantiate(enemy, new Vector3(transform.position.x, Random.Range(-8.0f, 8.0f), edgeVector.z+2f), Quaternion.identity);
             //enemyclone.velocity = -transform.forward*5;
-            count = 0;
+            
 
-            }
-        count++;
-        
+            }      
 
     }
 }
